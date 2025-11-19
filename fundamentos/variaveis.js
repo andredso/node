8c78147ef2d8065c779fd31fe4bbbd33f5b036b7
andredsoliv <axiscoord@outlook.com>
2025-11-19 12:25:10 -0300

    Node.js - Arquivos do Curso Avançado de JavaScript.

---
+++ fundamentos/variaveis.js
@@ -0,0 +1,31 @@
+/**
+ * Tutoria Node.js
+ * Variáveis
+ */
+
+console.log("______________________________const");
+const nome="Desenvolvimento";
+console.log(nome);
+console.log(typeof(nome));
+const pi=3.14;
+console.log(pi);
+console.log(typeof(pi));
+const vip=true;
+console.log(vip);
+console.log(typeof(vip));
+//pi=3.1415;
+console.log("______________________________var");
+var time="Corinthians";
+console.log(time);
+console.log(typeof(time));
+var time="Palmeiras";
+console.log(time);
+//problema no uso do var para declarar uma variável
+var time="São Paulo";
+console.log(time);
+console.log("______________________________let");
+let linguagem="Java";
+console.log(linguagem);
+linguagem="JavaScript";
+console.log(linguagem);
+//let linguagem="Python";
